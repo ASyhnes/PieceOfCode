fe56dff7d15f330e80aac0cc8c04a380f05fced9
ASyhnes <syhnes@gmail.com>
2024-04-09 18:45:19 +0200

    add C#

---
+++ C#/Entrainement/Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Variables
+{
+    class Program
+    {
+        
+
+        //fonctions
+        static void Main(string[] args) 
+        {
+            //variables
+            string name;
+            int age;
+            float score;
+
+            name = "Anthony";
+            Console.WriteLine("Hello " + name);
+            name = name + "Jeanjean";
+            Console.WriteLine(" Heallo " + name);
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Age = " + age);
+        }
+    }
+}
